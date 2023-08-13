c809f4b58ba29ed0675933a9c988e5488e325188
thesaurabhsingh <80765330+thesaurabhsingh@users.noreply.github.com>
2023-08-14 02:46:46 +0530

    updated

--- controllers/api/v1/users_api.js
+++ controllers/api/v1/users_api.js
@@ -1,47 +1,55 @@
-const Users = require('../../../models/users');
-const jwt = require('jsonwebtoken');
+const Users = require("../../../models/users");
+const jwt = require("jsonwebtoken");
 
-module.exports.signUp = async function(req,res){
-    //
-    try{
-        const user = await Users.create(req.body);
-        return res.status(200).json({
-            success: true,
-            message: "Users created successfully",
-        });
+module.exports.signUp = async function (req, res) {
+  //
+  try {
+    const existingUser = await Users.findOne({ email: req.body.email });
 
-    }catch(error){
-        return res.status(200).json({
-            success: false,
-            message: error.message
-        })
+    if (existingUser) {
+      return res.status(200).json({
+        success: true,
+        message: "An account with your email already exists !!",
+        user: existingUser,
+      });
     }
-}
 
-module.exports.createSession = async function(req,res){
-    
-    try{
-        let user = await Users.findOne({ email: req.body.email });
+    const user = await Users.create(req.body);
+    return res.status(200).json({
+      success: true,
+      message: "Account successfully created!",
+      user: user,
+    });
+  } catch (error) {
+    return res.status(200).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
-        if (!user || user.password != req.body.password) {
-          return res.status(422).json({
-            success: false,
-            message: "Invalid username or password",
-          });
-        }
+module.exports.createSession = async function (req, res) {
+  try {
+    let user = await Users.findOne({ email: req.body.email });
 
-        return res.status(200).json({
-          success: true,
-          message: "Sign in successful, here is your token please keep it safe",
-          data: {
-            token: jwt.sign(user.toJSON(), "api", { expiresIn: "100000000" }),
-          },
-        });
-
-    }catch(err){
-        return res.status(500).json({
-          success: false,
-          message: "Internal Server Error",
-        });
+    if (!user || user.password != req.body.password) {
+      return res.status(422).json({
+        success: false,
+        message: "Invalid username or password",
+      });
     }
-}
+
+    return res.status(200).json({
+      success: true,
+      message: "Sign in successful, here is your token please keep it safe",
+      data: {
+        token: jwt.sign(user.toJSON(), "api", { expiresIn: "100000000" }),
+      },
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
