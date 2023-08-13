08d95cc476b36504b98ae19b8c0455195e3508c8
thesaurabhsingh <80765330+thesaurabhsingh@users.noreply.github.com>
2023-08-13 20:20:07 +0530

    updated

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.connect(process.env.HUNGRYHUB);
-
+mongoose.set("strictQuery", true);
 const db = mongoose.connection;
 
 db.on('error',console.error.bind(console,"Error in connection db"));
